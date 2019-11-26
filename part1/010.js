0f5f4a296ee179cc4543518d7871146c754e97f1
Zetta Kim <zettabyte.kim@gmail.com>
2019-11-26 21:56:38 +0900

    first commit ~ 11

---
+++ part1/010.js
@@ -0,0 +1,9 @@
+var result = true;
+if (result) console.log('result가 참 입니다.');
+if (!result)
+    console.log('실행되지 않았습니다.');
+if (result) {
+    console.log('result의 결과');
+    console.log('>> 참 입니다.');
+}
+    
