c398381cbefba5c091e0c7f44ff76d57323b97d1
Zetta Kim <zettabyte.kim@gmail.com>
2019-12-01 10:34:07 +0900

    027 commit

---
+++ part2/022.js
@@ -0,0 +1,13 @@
+console.log(5 == 5);
+console.log("5" == 5);  // 형 변환하여 비교
+console.log(5 == '5');  // 형 변환하여 비교
+console.log('------');
+console.log(5 != 1);
+console.log(5 != "1");  // 형 변환하여 비교
+console.log(5 != "5");  // 형 변환하여 비교
+console.log('------');
+console.log(5 === 5);   // === 은 자료형 일치 여부까지 비교
+console.log(5 === "5");
+console.log('------');
+console.log(5 !== 10);  // !== 엄격한 기준, 값과 자료형 비교
+console.log(5 !== "5");
---
+++ part2/027.js
@@ -0,0 +1,10 @@
+var arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+console.log();
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log();
+console.log(arr[8]);
