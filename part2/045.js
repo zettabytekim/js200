4cfec23db26b6dff5ca5667b3af99bb6b38bda5a
Zetta Kim <zettabyte.kim@gmail.com>
2019-12-08 09:55:27 +0900

    045 commit

--- part2/045.js
+++ part2/045.js
@@ -3,3 +3,38 @@ let user = {
 };
 let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
 console.log(descriptor);
+
+let user2 = {};
+Object.defineProperty(user2, 'name', {
+    value: "zetta",
+    enumerable: true,
+    configurable: true,
+    writable: false
+});
+console.log(user2.name);
+user2.name = "bbo";
+console.log(user2.name);
+
+let user3 = {
+    name: "zetta",
+    toString() {
+        return this.name;
+    }
+};
+Object.defineProperty(user3, "toString", {
+    enumerable: false
+});
+for (let key in user3) {
+    console.log(key);
+}
+
+let user4 = {};
+Object.defineProperty(user4, "name", {
+    value: "zetta",
+    configurable: false
+});
+delete user4.name;
+console.log(user4.name);
+Object.defineProperty(user4, "name", {
+    writable: true
+});
